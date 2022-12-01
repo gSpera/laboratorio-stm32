89c8e5609fa616e62090936a5c04f3097eb7e3c7
Giovanni Spera <giovanni.spera2011@libero.it>
2022-12-01 17:51:00 +0100

    Add support for USART1

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #define REG(ADDR) (*(volatile unsigned int *)(ADDR))
+#define EXTRACT_BIT(VALUE, BIT_POS) (((VALUE) >> (BIT_POS)) & 1)
 
 #define GPIOA_BASE 0x48000000
+#define GPIOC_BASE 0x48000800
 #define GPIOE_BASE 0x48001000
 #define GPIO_MODER 0
 #define GPIO_ODR 0x14
+#define GPIO_AFRL 0x20
 #define RCC_BASE 0x40021000
 #define RCC_AHBENR 0x14
 #define RCC_APB1ENR 0x1C
+#define RCC_APB2ENR 0x18
 #define RCC_CFGR2 0x2C
 #define TIM6_BASE 0x40001000
 #define TIM_CR1 0x0
@@ -31,7 +35,16 @@
 #define ADCC12_BASE (ADC1_BASE + 0x300)
 #define ADCC_CCR 0x8
 
+#define USART1_BASE 0x40013800
+#define USART_CR1 0x00
+#define USART_CR2 0x04
+#define USART_BRR 0x0C
+#define USART_ISR 0x1C
+#define USART_RDR 0x24
+#define USART_TDR 0x28
+
 #define SYS_CLK 8000000  // 8MHz
+#define BAUD_RATE 9600
 
 void isr_tim6dacunder() {}
 
@@ -68,12 +81,21 @@ extern const unsigned int DATA_START;
 extern const unsigned int DATA_LEN;
 extern const unsigned int DATA_LOAD_ADDR;
 
-volatile int something = 0;
+void uart_putc(char ch) {
+    // Wait for TX clear
+    while (EXTRACT_BIT(REG(USART1_BASE | USART_ISR), 7) == 0)
+        ;
+    REG(USART1_BASE | USART_TDR) = ch;
+}
+
+void uart_put_dec(int v) {
+    while (v > 0) {
+        uart_putc((v % 10) + '0');
+        v /= 10;
+    }
+}
 
 int main() {
-    while (something == 0)
-        ;
-
     // Copy .data from flash to ram
     unsigned int *from = (unsigned int *)&DATA_LOAD_ADDR;
     unsigned int *to = (unsigned int *)&DATA_START;
@@ -81,33 +103,34 @@ int main() {
         to[i] = from[i];
     }
 
-    generate_buffer();
+    // generate_buffer();
 
     // Clock
-    REG(RCC_BASE | RCC_AHBENR) |= 1 << 21;
+    REG(RCC_BASE | RCC_AHBENR) |= (1 << 19) | (1 << 21);  // Port C | Port E
     REG(RCC_BASE | RCC_APB1ENR) |= 1 << 4;
+    REG(RCC_BASE | RCC_APB2ENR) |= 1 << 14;  // USART1
 
     // GPIO
+    REG(GPIOC_BASE | GPIO_MODER) |= (0b10 << 8) | (0b10 << 10);
+    REG(GPIOC_BASE | GPIO_AFRL) |= (0b0111 << 16) | (0b0111 << 20);
     REG(GPIOE_BASE | GPIO_MODER) |= 0x5555 << 16;
+
+    // UART
+    REG(USART1_BASE | USART_CR1) = (1 << 3) | (1 << 2);  // TE | RE
+    REG(USART1_BASE | USART_CR2) = (0b00 << 12);         // 1 Stop Bit
+    REG(USART1_BASE | USART_BRR) = SYS_CLK / BAUD_RATE;
+    REG(USART1_BASE | USART_CR1) |= 1 << 0;  // Enable
+
+    uart_putc('A');
+    uart_putc('B');
+    uart_put_dec(785);
+    uart_putc('X');
 
     // Freq = SYS_CLOCK / (PSC + 1) = freq
     // SYS_CLOCK / freq -1 = PSC
     REG(TIM6_BASE | TIM_PSC) = 8000 - 1;  // Freq: 1kHz
     REG(TIM6_BASE | TIM_ARR) = 1000;      // Freq: freq
     REG(TIM6_BASE | TIM_CR1) |= 1 << 0;
-
-    int i = 0;
-    while (1) {
-        REG(GPIOE_BASE | GPIO_ODR) |= buffer[i] << 8;
-
-        while (1) {
-            int sr = REG(TIM6_BASE | TIM_SR);
-            if (sr != 0) break;
-        }
-        REG(TIM6_BASE | TIM_SR) = 1;
-        i++;
-        i %= 4096;
-    }
 
     while (1)
         ;
